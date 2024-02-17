ec2e360d195df84f2e386dc76fc7f06046cc0370
Tonkooo <at.antonio.todorov@gmail.com>
2024-02-17 22:48:08 +0200

    end of workshop 1 - creating common and photos models

--- softuni_petstagram/common/models.py
+++ softuni_petstagram/common/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from softuni_petstagram.photos.models import PetPhoto
+
+'''
+The field Comment Text is required:
+•	Comment Text - it should consist of a maximum of 300 characters.
+An additional field should be created:
+•	Date and Time of Publication - when a comment is created (only), the date of publication is automatically generated.
+One more thing we should keep in mind is that the comment should relate to the photo (as in social apps users comment on a specific photo/post, i.e., the comment object is always connected to the photo object).
+Open the common/models.py file and let us create the model: 
+'''
+class PhotoComment(models.Model):
+    MAX_TEXT_LENGTH = 300
+
+    text = models.TextField(
+        max_length=MAX_TEXT_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True,)
+
+    modified_at = models.DateTimeField(auto_now=True, )
+
+    pet_photo = models.ForeignKey(PetPhoto, on_delete=models.DO_NOTHING)
+
+class PhotoLike(models.Model):
+    pet_photo = models.ForeignKey(PetPhoto, on_delete=models.DO_NOTHING)
--- softuni_petstagram/photos/admin.py
+++ softuni_petstagram/photos/admin.py
@@ -5,5 +5,11 @@ from softuni_petstagram.photos.models import PetPhoto
 
 @admin.register(PetPhoto)
 class PetPhotoAdmin(admin.ModelAdmin):
-    #list_display = ('photo', 'description', 'location')
-    pass
+    list_display = ('short_description', 'pk', 'location', 'created_at', 'tagged_pets')
+
+    def short_description(self, obj):
+        return obj.description[:25]
+
+    def tagged_pets(self, obj):
+        return ', '.join(pet.name for pet in obj.pets.all())
+
