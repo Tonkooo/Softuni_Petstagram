4bad0ea5c7a64f36c66e2464fa8d6408d0c206a7
Tonkooo <at.antonio.todorov@gmail.com>
2024-02-20 17:04:56 +0200

    optimize the project by creating templates

--- softuni_petstagram/common/views.py
+++ softuni_petstagram/common/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from softuni_petstagram.photos.models import PetPhoto
+
 
 def index(request):
-    context = {}
+    context = {
+        'pet_photos': PetPhoto.objects.all(),
+
+    }
     return render(request, 'common/index.html', context)
